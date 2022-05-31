9759f0438087c2e4fc385d34adfee3edaabbba1f
Jyoti Ranjan Majumdar <67379705+jyotiranjan-majumdar@users.noreply.github.com>
2022-05-31 15:47:45 +0530

    first commit

---
+++ main/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . models import Task
+# Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    
+    list_display = ['title']
+    class Meta:
+        model = Task
+
+admin.site.register(Task, TaskAdmin)
